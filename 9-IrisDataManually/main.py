74dbaca0d31caf90fde7419bb311a94617f5ac9d
nicolalandro <nicolaxx94@live.it>
2018-04-19 18:36:41 +0200

    ripristinate modifiche

--- 9-IrisDataManually/main.py
+++ 9-IrisDataManually/main.py
@@ -1,16 +1,32 @@
 import pandas as pd
-import tensorflow as tf
 
 CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth',
                     'PetalLength', 'PetalWidth', 'Species']
 SPECIES = ['Setosa', 'Versicolor', 'Virginica']
 
-train = pd.read_csv('iris_training.csv', names=CSV_COLUMN_NAMES)
-test = pd.read_csv('iris_test.csv', names=CSV_COLUMN_NAMES)
+train_csv = pd.read_csv('iris_training.csv', names=CSV_COLUMN_NAMES)
+train_examples = train_csv.drop(['Species'], axis=1).values
+train_truths = train_csv['Species'].get_values()
 
-for index, row in train.iterrows():
-    try:
-        numeric_species = int(row['Species'])
-        print SPECIES[numeric_species] 
-    except:
-        print row['Species']
+test_csv = pd.read_csv('iris_test.csv', names=CSV_COLUMN_NAMES)
+test_examples = test_csv.drop(['Species'], axis=1).values
+test_truths = test_csv['Species'].get_values()
+
+# SKlearn Area
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier()
+clf.fit(train_examples, train_truths)
+prediction = clf.predict(test_examples)
+print("result with decision tree:", accuracy_score(test_truths, prediction))
+
+# Tensorflow Area
+import tensorflow as tf 
+
+
+# for index, row in train_csv.iterrows():
+#     try:
+#         numeric_species = int(row['Species'])
+#         print(SPECIES[numeric_species])
+#     except:
+#         print(row['Species'])
