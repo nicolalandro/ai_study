2f65658da5ce873ec8eea8b2fbbf680e26a33090
nicolalandro <nicolaxx94@live.it>
2018-04-22 12:15:39 +0200

    wip neural networks

--- 9-IrisDataManually/main.py
+++ 9-IrisDataManually/main.py
@@ -15,14 +15,37 @@ test_truths = test_csv['Species'].get_values()
 # SKlearn Area
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+
 clf = DecisionTreeClassifier()
 clf.fit(train_examples, train_truths)
 prediction = clf.predict(test_examples)
 print("result with decision tree:", accuracy_score(test_truths, prediction))
 
-# Tensorflow Area
-import tensorflow as tf 
+# keras Area
+import keras
+from keras.models import Sequential
 
+model = Sequential()
+from keras.layers import Dense
+
+model.add(Dense(units=8, activation='relu', input_dim=4))
+model.add(Dense(units=16, activation='relu', input_dim=4))
+model.add(Dense(units=3, activation='softmax'))
+
+model.compile(loss=keras.losses.sparse_categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+
+model.fit(train_examples, train_truths, epochs=20, batch_size=32)
+
+loss_and_metrics = model.evaluate(test_examples, test_truths, batch_size=128)
+print("loss and metrics: ", loss_and_metrics)
+
+prediction = model.predict(test_truths, batch_size=128)
+prediction = prediction.argmax(1)
+
+from sklearn.metrics import accuracy_score
+
+print("result with neural network: ", accuracy_score(test_truths, prediction))
 
 # for index, row in train_csv.iterrows():
 #     try:
