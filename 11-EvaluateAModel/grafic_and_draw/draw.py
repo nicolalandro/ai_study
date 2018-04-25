41b5c4848383228a0e2834408d4b70e221534538
nicolalandro <nicolaxx94@live.it>
2018-04-25 19:35:47 +0200

    boh

--- 11-EvaluateAModel/grafic_and_draw/draw.py
+++ 11-EvaluateAModel/grafic_and_draw/draw.py
@@ -1,13 +1,6 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-from sklearn import metrics
-from sklearn.metrics import roc_curve, f1_score, accuracy_score, precision_recall_curve, classification_report, \
-    confusion_matrix
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
 
 pd.set_option('max.columns', None)
 
@@ -18,5 +11,26 @@ print('Legendary:', str(len(df[df['Legendary'] == True]) / len(df) * 100) + '%')
 
 plt.title('Count Plot')
 plt.xticks(rotation=45)
-sns_plot = sns.countplot(df['Type 1'])
+sns_plot_type1 = sns.countplot(df['Type 1'])
+# sns_plot.figure.savefig("figure.png")
+plt.close()
+
+plt.title('Count Plot')
+plt.xticks(rotation=45)
+sns_plot_type2 = sns.countplot(df['Type 2'])
+plt.close()
+
+sns_plot_tot = sns.distplot(df['Total'])
+plt.close()
+
+sns_plot_x = sns.pairplot(df[['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']])
+# sns_plot.savefig("figure.png")
+plt.close()
+
+corr = df.corr()
+sns_plot = sns.heatmap(corr, cmap='coolwarm', annot=True)
 sns_plot.figure.savefig("figure.png")
+plt.close()
+
+# sns.heatmap(corr, annot=True)
+# plt.savefig("heatmap.png")
