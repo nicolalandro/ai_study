1a59979034b8948f2d108c3f4224418628c0f208
nicolalandro <nicolaxx94@live.it>
2018-04-25 19:41:27 +0200

    print buona

--- 11-EvaluateAModel/main.py
+++ 11-EvaluateAModel/main.py
@@ -4,6 +4,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
+import matplotlib.pyplot as plt
 
 iris = load_iris()
 x = iris.data
@@ -17,14 +18,19 @@ decision_tree = DecisionTreeClassifier()
 decision_tree.fit(x_train, y_train)
 decision_tree_predictions = decision_tree.predict(x_test)
 
+plt.close()
+
 cnf_matrix = confusion_matrix(y_test, decision_tree_predictions)
-sns_plot = seaborn.heatmap(cnf_matrix, annot=True, fmt="d", center=0)
+sns_plot = seaborn.heatmap(cnf_matrix, annot=True, center=0)
 sns_plot.figure.savefig("cnf_matrix.png")
+plt.close()
+
 
 normalized_cnf_matrix = cnf_matrix.astype('float') / cnf_matrix.sum(axis=1)[:, np.newaxis]
-print(normalized_cnf_matrix)
 sns_plot = seaborn.heatmap(normalized_cnf_matrix, annot=True, center=0)
 sns_plot.figure.savefig("normalized_cnf_matrix.png")
+plt.close()
+
 
 print("decision tree perecision: ", accuracy_score(y_test, decision_tree_predictions))
 print("decision tree recal macro: ", recall_score(y_test, decision_tree_predictions, average='macro'))
