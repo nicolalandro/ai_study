c59c3845c6e4add57a8b05107ec3566bf6e78827
nicolalandro <nicolaxx94@live.it>
2018-04-25 16:13:57 +0200

    recall

--- 11-EvaluateAModel/main.py
+++ 11-EvaluateAModel/main.py
@@ -1,9 +1,8 @@
+import seaborn
 from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-import seaborn
-import matplotlib.pyplot
 
 iris = load_iris()
 x = iris.data
@@ -13,13 +12,16 @@ sns_plot.figure.savefig("rating.png")
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
 
-
 decision_tree = DecisionTreeClassifier()
 decision_tree.fit(x_train, y_train)
 decision_tree_predictions = decision_tree.predict(x_test)
 
-
 cnf_matrix = confusion_matrix(y_test, decision_tree_predictions)
 sns_plot = seaborn.heatmap(cnf_matrix, annot=True, fmt="d", center=0)
 sns_plot.figure.savefig("cnf_matrix.png")
+
 print("decision tree perecision: ", accuracy_score(y_test, decision_tree_predictions))
+print("decision tree recal macro: ", recall_score(y_test, decision_tree_predictions, average='macro'))
+print("decision tree recal micro: ", recall_score(y_test, decision_tree_predictions, average='micro'))
+print("decision tree recal weighted: ", recall_score(y_test, decision_tree_predictions, average='weighted'))
+print("decision tree recal none: ", recall_score(y_test, decision_tree_predictions, average=None))
