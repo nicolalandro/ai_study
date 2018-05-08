1b1e11b882ee5f5918193c63f90f7506628fa95d
nicolalandro <nicolaxx94@live.it>
2018-05-08 18:55:09 +0200

    media

--- 13-ImageRecognition/flowers_sample/dataset_analysis.py
+++ 13-ImageRecognition/flowers_sample/dataset_analysis.py
@@ -3,23 +3,11 @@ import os
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import seaborn
 from PIL import Image
 
 base_dir = '../flowers'
 classes_folders = os.listdir(base_dir)
-# print('classes folders: ', len(classes_folders))
-files_name_for_class = []
-counter = []
-for idx, class_name in enumerate(classes_folders):
-    path = os.path.join(base_dir, class_name, '*.jpg')
-    files_name = glob.glob(path)
-    files_name_for_class.append(files_name)
-    counter.append(len(files_name))
-    # print(classes_folders[idx], len(files_name))
-
-fig = plt.bar(classes_folders, counter)
-plt.savefig("0-daset_number.png")
-plt.close()
 
 dict_s = {
     'file_path': [],
@@ -43,4 +31,9 @@ for class_name in classes_folders:
         dict_s['color_channels'].append(im.mode)
 
 pd_value = pd.DataFrame.from_dict(dict_s)
-print(pd_value)
+
+print(pd_value[['h', 'w', 'size']].agg(['mean']))
+# print(pd_value.groupby('class_name').size())
+# sns_plot = seaborn.countplot(pd_value.groupby('class_name').size())
+# sns_plot.figure.savefig("rating.png")
+# plt.close()
