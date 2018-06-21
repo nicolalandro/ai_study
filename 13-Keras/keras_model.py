0670436c159310c5cf8140722953db8d2ff371a4
nicolalandro <nicolaxx94@live.it>
2018-06-21 19:11:54 +0200

    keras classifier for text

---
+++ 13-Keras/keras_model.py
@@ -0,0 +1,20 @@
+import keras
+from keras import Sequential, Input
+from keras.layers import Dense
+
+
+class KerasModel:
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = Sequential()
+        self.model.add(Dense(units=32, activation='relu', input_dim=4000))
+        self.model.add(Dense(units=16, activation='relu', input_dim=32))
+        self.model.add(Dense(units=23, activation='softmax'))
+        self.model.compile(loss=keras.losses.sparse_categorical_crossentropy,
+                           optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+
+    def fit(self, x, y):
+        self.model.fit(x, y, epochs=20, batch_size=32)
+
+    def predict(self, x):
+        return self.model.predict(x, batch_size=128)
---
+++ 13-Keras/main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from keras_model import KerasModel
+from sklearn.preprocessing import LabelEncoder
+
+examples = []
+truths = []
+
+dataset = pd.read_csv('../12-SklearnPipeline/makeup_dataset.csv', sep='|')
+for index, row in dataset.iterrows():
+    examples.append(str(row[1]) + ' ' + str(row[2]))
+    truths.append(str(row[4]))
+
+train_examples, test_examples, train_truths, test_truths = train_test_split(examples, truths, test_size=0.33)
+
+text_clf_extra_tree = Pipeline([('vect', CountVectorizer()),
+                                ('clf-extra-tree',
+                                 ExtraTreesClassifier(n_estimators=100, n_jobs=12, bootstrap=False, min_samples_split=2,
+                                                      random_state=0))
+                                ])
+text_clf_extra_tree.fit(train_examples, train_truths)
+text_clf_prediction = text_clf_extra_tree.predict(test_examples)
+
+print("Extra tree with count vectorizer perecision: ", accuracy_score(test_truths, text_clf_prediction))
+
+le = LabelEncoder()
+le.fit(train_truths)
+train_truths = le.transform(train_truths)
+
+clf = Pipeline([('vect', TfidfVectorizer(use_idf=True, max_features=4000)),
+                ('clf-keras', KerasModel())
+                ])
+clf.fit(train_examples, train_truths)
+pred = clf.predict(test_examples).argmax(1)
+
+print("Extra tree with count vectorizer perecision: ", accuracy_score(le.transform(test_truths), pred))
